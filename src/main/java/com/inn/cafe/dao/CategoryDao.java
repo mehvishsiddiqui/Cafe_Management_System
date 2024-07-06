fc12713be88c325bf92b81df63829cb5f6b7cfd6
mehvishsiddiqui <mehvishsiddiqui88@gmail.com>
2024-07-06 14:49:53 +0400

    commit

---
+++ src/main/java/com/inn/cafe/dao/CategoryDao.java
@@ -0,0 +1,16 @@
+package com.inn.cafe.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.inn.cafe.POJO.Category;
+
+@Repository
+public interface CategoryDao extends JpaRepository<Category , Integer> {
+
+	List<Category> getAllCategory();
+	
+	
+}
